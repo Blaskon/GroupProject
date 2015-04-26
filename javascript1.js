f52f24313dc4dade7b92c54d1d1cf38adfb3da40
Miguel <josmi.king@gmail.com>
2015-04-26 20:36:52 +0100

    Fixed contact form

--- javascript1.js
+++ javascript1.js
@@ -1,33 +1,33 @@
-
 function contact(){
-
-    //get references
-    
-    
-    var Name = document.getElementById('name');
-  
-    var Telephone = document.getElementById('telephone');
-    var Email = document.getElementById('email');
-	var Enquiry = document.getElementById('enquiry');        
-    
-    //Validation
-    
-    if(name.value == null || name.value=="") {
-        alert("Please enter a name");
-        return false;
-       }
-	else if(telephone.value == null || telephone.value == ""){
-        alert("Please enter a phone number");
-        return false;
-    }
-        else if(email.value == "" || email.value == null || email.value.indexOf("@")<1){
-        alert("Please enter valid email");
-        return false;
-    }
-        else if(enquiry.value == null || enquiry.value == ""){
-        alert("Please enter enquiry");
-        return false;
-    }
-    
-    
+	
+	// Get users input
+	var name = document.getElementById("userName").value;
+	var telephone = document.getElementById("userPhone").value;
+	var email = document.getElementById("userEmail").value;
+	
+	// Regular expressions to validate fields
+	var nameRegEx = /^[a-zA-Z]+$/;
+	var phoneRegEx = /^\d+$/;
+	var emailRegEx = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;
+	
+	// Validation logic
+	
+	//alert("This is your name " + name);
+	if((name === "") || (name === null) || (!name.match(nameRegEx))){
+		alert("Please enter a valid name");
+		//matches("^[\\p{L} .'-]+$")
+		return false;
+	}
+	if(!telephone.match(phoneRegEx)){
+		alert("Please enter a valid number");
+		return false;
+	}
+	
+	if(!email.match(emailRegEx)){
+		alert("Please enter a valid email");
+		return false;
+	}
+	
+	document.getElementById("myForm").submit();
+	return true;
 }
